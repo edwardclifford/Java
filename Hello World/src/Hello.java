892bfaa6fd9cb930675c27ec39e316fac7de8201
Ted Clifford <36415841+edwardclifford@users.noreply.github.com>
2018-02-27 09:06:45 -0500

    Add files via upload

---
+++ Hello World/src/Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		String day = "Tuesday";
+		int date = 9;
+		String month = "January";
+		int year = 2018;
+		System.out.println("American Format:");
+		System.out.println("Today is " + day + ", " + month + " " + date + ", " + year);
+		System.out.println("European Format:");
+		System.out.println(day + " " + date + " " + month + " " + year);
+	
+		
+	}
+
+}
